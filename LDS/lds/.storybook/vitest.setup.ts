f63a7462445e8c626f42997b97e9a589a963146d
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2025-09-24 09:02:53 +0900

    .

--- LDS/lds/.storybook/vitest.setup.ts
+++
@@ -1,6 +0,0 @@
-import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
-import { setProjectAnnotations } from '@storybook/react';
-import * as projectAnnotations from './preview';
-
-// 스토리북 a11y + 프로젝트 프리뷰 설정을 Vitest 환경에도 적용
-setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
